dd9eed4698edff4ae9cf8a8da397dd6fba5d9ada
zhengshuxin <zhengshuxin@gmail.com>
2015-12-13 10:15:00 +0800

    compiling acl ok on FreeBSD

--- lib_acl_cpp/src/stdlib/charset_conv.cpp
+++ lib_acl_cpp/src/stdlib/charset_conv.cpp
@@ -271,7 +271,7 @@ bool charset_conv::update_begin(const char* fromCharset,
 #elif defined(ACL_SUNOS5)
 		__iconv(m_iconv, (const char**) &pNil, &zero, &pNil, &zero);
 #elif defined(ACL_FREEBSD)
-		__iconv(m_iconv, (char**) &pNil, &zero, &pNil, &zero);
+		__iconv(m_iconv, (const char**) &pNil, &zero, &pNil, &zero);
 #else
 		__iconv(m_iconv, &pNil, &zero, &pNil, &zero);
 #endif
@@ -367,7 +367,7 @@ bool charset_conv::update(const char* in, size_t len, acl::string* out)
 #elif defined(ACL_SUNOS5)
 		ret = __iconv(m_iconv, (const char**) &pIn, &nIn, &pOut, &nOut);
 #elif defined(ACL_FREEBSD)
-		ret = __iconv(m_iconv, (char**) &pIn, &nIn, &pOut, &nOut);
+		ret = __iconv(m_iconv, (const char**) &pIn, &nIn, &pOut, &nOut);
 #else
 		ret = __iconv(m_iconv, &pIn, &nIn, &pOut, &nOut);
 #endif
@@ -409,7 +409,7 @@ bool charset_conv::update(const char* in, size_t len, acl::string* out)
 			__iconv(m_iconv, (const char**) &pNil,
 				&zero, &pNil, &zero);
 #elif defined(ACL_FREEBSD)
-			__iconv(m_iconv, (char**) &pNil, &zero, &pNil, &zero);
+			__iconv(m_iconv, (const char**) &pNil, &zero, &pNil, &zero);
 #else
 			__iconv(m_iconv, &pNil, &zero, &pNil, &zero);
 #endif
@@ -455,7 +455,7 @@ bool charset_conv::update(const char* in, size_t len, acl::string* out)
 #elif defined(ACL_SUNOS5)
 			__iconv(m_iconv, (const char**) &pNil, &zero, &pNil, &zero);
 #elif defined(ACL_FREEBSD)
-			__iconv(m_iconv, (char**) &pNil, &zero, &pNil, &zero);
+			__iconv(m_iconv, (const char**) &pNil, &zero, &pNil, &zero);
 #else
 			__iconv(m_iconv, &pNil, &zero, &pNil, &zero);
 #endif
