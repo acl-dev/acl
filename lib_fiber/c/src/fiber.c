b91d15cfedd87e6b275d8f615ecfb4c438f9edcf
zhengshuxin <zhengshuxin@gmail.com>
2016-10-31 14:04:43 +0800

    add LONGJMP/SETJMP in asm language.

--- lib_fiber/c/src/fiber.c
+++ lib_fiber/c/src/fiber.c
@@ -200,6 +200,7 @@ void fiber_save_errno(void)
 }
 
 #if defined(__x86_64__)
+
 # if defined(__AVX__)
 #  define CLOBBER \
         , "ymm0", "ymm1", "ymm2", "ymm3", "ymm4", "ymm5", "ymm6", "ymm7",\
@@ -235,21 +236,50 @@ void fiber_save_errno(void)
 
 # define LONGJMP(ctx) \
     asm("movq   (%%rax), %%rbx\n\t"\
-	    "movq   8(%%rax), %%rbp\n\t"\
-	    "movq   16(%%rax), %%r12\n\t"\
-	    "movq   24(%%rax), %%rdx\n\t"\
-	    "movq   32(%%rax), %%r13\n\t"\
-	    "movq   40(%%rax), %%r14\n\t"\
-	    "mov    %%rdx, %%rsp\n\t"\
-	    "movq   48(%%rax), %%r15\n\t"\
-	    "movq   56(%%rax), %%rdx\n\t"\
-	    "movq   64(%%rax), %%rdi\n\t"\
-	    "movq   72(%%rax), %%rsi\n\t"\
-	    "jmp    *%%rdx\n\t"\
+	"movq   8(%%rax), %%rbp\n\t"\
+	"movq   16(%%rax), %%r12\n\t"\
+	"movq   24(%%rax), %%rdx\n\t"\
+	"movq   32(%%rax), %%r13\n\t"\
+	"movq   40(%%rax), %%r14\n\t"\
+	"mov    %%rdx, %%rsp\n\t"\
+	"movq   48(%%rax), %%r15\n\t"\
+	"movq   56(%%rax), %%rdx\n\t"\
+	"movq   64(%%rax), %%rdi\n\t"\
+	"movq   72(%%rax), %%rsi\n\t"\
+	"jmp    *%%rdx\n\t"\
         : : "a" (ctx) : "rdx" \
     )
 
+#elif defined(__i386__)
+
+# define SETJMP(ctx) ({\
+    int ret;\
+    asm("movl   $LJMPRET%=, %%eax\n\t"\
+	"movl   %%eax, (%%edx)\n\t"\
+	"movl   %%ebx, 4(%%edx)\n\t"\
+	"movl   %%esi, 8(%%edx)\n\t"\
+	"movl   %%edi, 12(%%edx)\n\t"\
+	"movl   %%ebp, 16(%%edx)\n\t"\
+	"movl   %%esp, 20(%%edx)\n\t"\
+	"xorl   %%eax, %%eax\n\t"\
+	"LJMPRET%=:\n\t"\
+	: "=a" (ret) : "d" (ctx) : "memory");\
+	ret;\
+    })
+
+# define LONGJMP(ctx) \
+    asm("movl   (%%eax), %%edx\n\t"\
+	"movl   4(%%eax), %%ebx\n\t"\
+	"movl   8(%%eax), %%esi\n\t"\
+	"movl   12(%%eax), %%edi\n\t"\
+	"movl   16(%%eax), %%ebp\n\t"\
+	"movl   20(%%eax), %%esp\n\t"\
+	"jmp    *%%edx\n\t"\
+	: : "a" (ctx) : "edx" \
+   )
+
 #else
+
 # define SETJMP(ctx) \
     sigsetjmp(ctx, 0)
 # define LONGJMP(ctx) \
