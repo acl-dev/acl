aedcf2de100989875183aabc43c793ec8cc0e1ca
zhengshuxin <zhengshuxin@gmail.com>
2017-06-12 23:24:00 +0800

    for new acl-master lib

---
+++ lib_acl/include/master/acl_master_flow.h
@@ -0,0 +1,28 @@
+#ifndef __ACL_MASTER_FLOW_INCLUDED_H__
+#define __ACL_MASTER_FLOW_INCLUDED_H__
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "../stdlib/acl_define.h"
+
+#ifdef ACL_UNIX
+
+ /*
+  * Functional interface.
+  */
+extern int acl_var_master_flow_pipe[2];
+
+extern void acl_master_flow_init(void);
+extern int acl_master_flow_get(int);
+extern int acl_master_flow_put(int);
+extern int acl_master_flow_count(void);
+
+#endif /* ACL_UNIX */
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif
