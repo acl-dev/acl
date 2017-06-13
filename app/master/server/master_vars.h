65cd3b94ff1c9c55c30f496f48705265387c9a31
zhengshuxin <zhengshuxin@gmail.com>
2017-06-13 21:55:40 +0800

    rm master dir

--- app/master/server/master_vars.h
+++
@@ -1,9 +0,0 @@
-#ifndef MASTER_VARS_INCLUDE_H
-#define MASTER_VARS_INCLUDE_H
-
-extern acl_pthread_pool_t *acl_var_master_thread_pool;
-
-void acl_master_vars_init(int buf_size, int rw_timeout);
-void acl_master_vars_end(void);
-
-#endif
--- app/master/stdafx.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STDAFX_INCLUDE_H_
-#define __STDAFX_INCLUDE_H_
-
-#include "lib_acl.h"
-#include "lib_protocol.h"
-
-#endif
