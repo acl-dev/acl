d52e8c5113a915d24d342f245490144438c88004
Zheng Shuxin <zhengshuxin@qiyi.com>
2017-07-03 16:00:05 +0800

    modify lib_fiber module

--- lib_fiber/c/src/event.c
+++ lib_fiber/c/src/event.c
@@ -429,7 +429,7 @@ int event_process(EVENT *ev, int timeout)
 	/* limit the event wait time just for fiber schedule exiting
 	 * quickly when no tasks left
 	 */
-	if (timeout > 1000)
+	if (timeout > 1000 || timeout <= 0)
 		timeout = 1000;
 
 #ifdef	DEL_DELAY
