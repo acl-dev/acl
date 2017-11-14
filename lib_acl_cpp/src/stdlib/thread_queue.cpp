781b0a048ae6a7501de1f8780b7b53e677ec2b3d
zhengshuxin <zhengshuxin@qiyi.com>
2017-11-14 10:52:44 +0800

    add acl::tbox class for transfering messages between multi-threads.

--- lib_acl_cpp/src/stdlib/thread_queue.cpp
+++ lib_acl_cpp/src/stdlib/thread_queue.cpp
@@ -40,4 +40,34 @@ int thread_queue::qlen() const
 	return acl_aqueue_qlen(queue_);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+
+void* tbox_create(void)
+{
+	return acl_aqueue_new();
+}
+
+void tbox_free(void* tbox, void (*free_fn)(void*))
+{
+	acl_aqueue_free((ACL_AQUEUE*) tbox, free_fn);
+}
+
+bool tbox_push(void* tbox, void* o)
+{
+	return acl_aqueue_push((ACL_AQUEUE*) tbox, o) == -1 ? false : true;
+}
+
+void* tbox_pop(void* tbox, int wait_ms /* = -1 */)
+{
+	int wait_sec = wait_ms / 1000;
+	int wait_usec = (wait_ms % 1000) * 1000;
+	return acl_aqueue_pop_timedwait((ACL_AQUEUE*) tbox,
+			wait_sec, wait_usec);
+}
+
+size_t tbox_size(void* tbox)
+{
+	return (size_t) acl_aqueue_qlen((ACL_AQUEUE*) tbox);
+}
+
 } // namespace acl
