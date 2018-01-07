2b9cdae22e8b0f35f40916e0d49aaa1eb5e576a5
zsxxsz <zhengshuxin@gmail.com>
2018-01-07 14:33:05 +0800

    add sane_socket in fiber's common module.

---
+++ lib_fiber/c/src/common/sane_socket.c
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "sane_socket.h"
+
+int is_listen_socket(SOCKET fd)
+{
+	int val, ret;
+#ifdef SYS_WIN
+	int len = sizeof(val);
+#else
+	socklen_t len = sizeof(val);
+#endif
+
+	ret = getsockopt(fd, SOL_SOCKET, SO_ACCEPTCONN, (void*) &val, &len);
+	if (ret == -1) {
+		return 0;
+	} else if (val) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
---
+++ lib_fiber/c/src/common/sane_socket.h
@@ -0,0 +1,6 @@
+#ifndef SANE_SOCKET_INCLUDE_H
+#define SANE_SOCKET_INCLUDE_H
+
+int is_listen_socket(SOCKET fd);
+
+#endif
