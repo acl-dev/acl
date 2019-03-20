c5c2659eb37d46d021cd4a5ee3369c2926c9b3ef
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2019-03-20 13:55:17 +0800

    rename chunk_chain.cpp.cpp to chunk_chain.cpp

--- lib_acl/samples/chunk_chain/chunk_chain.cpp
+++ lib_acl/samples/chunk_chain/chunk_chain.cpp
@@ -1,4 +1,4 @@
-// chunk_chain.cpp.cpp : 定义控制台应用程序的入口点。
+// .cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
--- lib_acl/samples/chunk_chain/stdafx.cpp
+++ lib_acl/samples/chunk_chain/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : 只包括标准包含文件的源文件
-// chunk_chain.cpp.pch 将成为预编译头
+// chunk_chain.pch 将成为预编译头
 // stdafx.obj 将包含预编译类型信息
 
 #include "stdafx.h"
