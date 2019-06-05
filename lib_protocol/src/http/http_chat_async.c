287c642ff41aabe9c677f8b8ace20b61748713d2
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2019-06-05 15:07:53 +0800

    fixed bugs in async reading body data from webserver.

--- lib_protocol/src/http/http_chat_async.c
+++ lib_protocol/src/http/http_chat_async.c
@@ -79,29 +79,32 @@ static int hdr_ready(HTTP_HDR *hdr, char *line, int dlen)
 	if (hdr->max_lines > 0 && hdr->cur_lines > hdr->max_lines) {
 		acl_msg_error("%s(%d): cur_lines(%d) > max_lines(%d)",
 			myname, __LINE__, hdr->cur_lines, hdr->max_lines);
-		return (HTTP_CHAT_ERR_TOO_MANY_LINES);
+		return HTTP_CHAT_ERR_TOO_MANY_LINES;
 	}
 
-	if (dlen > 0)
+	if (dlen > 0) {
 		hdr->valid_lines++;
+	}
 
 	if (dlen == 0) {
-		if (hdr->valid_lines > 0)
-			return (HTTP_CHAT_OK);
-		else
-			return (HTTP_CHAT_CONTINUE);
+		if (hdr->valid_lines > 0) {
+			return HTTP_CHAT_OK;
+		} else {
+			return HTTP_CHAT_CONTINUE;
+		}
 	}
 
-	if (hdr->valid_lines == 1)
+	if (hdr->valid_lines == 1) {
 		entry = http_hdr_entry_head(line);
-	else
+	} else {
 		entry = http_hdr_entry_new2(line);
+	}
 	if (entry == NULL) { /* ignore invalid entry line */
-		return (HTTP_CHAT_CONTINUE);
+		return HTTP_CHAT_CONTINUE;
 	}
 
 	http_hdr_append_entry(hdr, entry);
-	return (HTTP_CHAT_CONTINUE);
+	return HTTP_CHAT_CONTINUE;
 }
 
 /* 异步读取一行数据的回调函数  */
@@ -160,7 +163,7 @@ static int hdr_can_read(ACL_ASTREAM *astream, void *context)
 	while (1) {
 		if ((ret = acl_aio_can_read(astream)) == ACL_VSTREAM_EOF) {
 			notify(HTTP_CHAT_ERR_IO, arg);
-			return (-1);
+			return -1;
 		} else if (ret == 0) {
 			break;
 		}
@@ -179,18 +182,18 @@ static int hdr_can_read(ACL_ASTREAM *astream, void *context)
 		case HTTP_CHAT_OK:
 			DISABLE_READ(astream);
 			if (notify(ret, arg) < 0) {
-				return (0);
+				return 0;
 			}
-			return (0);
+			return 0;
 		default:
 			DISABLE_READ(astream);
 			(void) notify(ret, arg);
-			return (0);
+			return 0;
 		}
 	}
 
 	acl_aio_enable_read(astream, hdr_can_read, ctx);
-	return (0);
+	return 0;
 }
 
 /* 异步读取一个完整的HTTP协议头 */
@@ -202,8 +205,9 @@ static void hdr_get_async(ctx_type type, HTTP_HDR *hdr, ACL_ASTREAM *astream,
 	HTTP_CHAT_CTX *ctx;
 	ACL_VSTREAM *stream;
 
-	if (hdr == NULL || astream == NULL || notify == NULL)
+	if (hdr == NULL || astream == NULL || notify == NULL) {
 		acl_msg_fatal("%s: input invalid", myname);
+	}
 
 	stream = acl_aio_vstream(astream);
 	stream->rw_timeout = 0; /* bug fix, --zsx, 2007.7.7 */
@@ -212,12 +216,12 @@ static void hdr_get_async(ctx_type type, HTTP_HDR *hdr, ACL_ASTREAM *astream,
 		hdr->chat_ctx = new_ctx(type);
 		hdr->chat_free_ctx_fn = free_ctx_fn;
 	}
-	ctx = (HTTP_CHAT_CTX*) hdr->chat_ctx;
-	ctx->hdr = hdr;
-	ctx->timeout = timeout;
+	ctx                    = (HTTP_CHAT_CTX*) hdr->chat_ctx;
+	ctx->hdr               = hdr;
+	ctx->timeout           = timeout;
 	ctx->notify.hdr_notify = notify;
-	ctx->arg = arg;
-	ctx->status = CHAT_S_HDR;
+	ctx->arg               = arg;
+	ctx->status            = CHAT_S_HDR;
 
 #if 0
     acl_aio_ctl(astream,
@@ -257,8 +261,9 @@ static int chunked_data_endl(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 	int   dlen;
 
 	sbuf = acl_aio_gets_peek(astream);
-	if (sbuf == NULL)
-		return (0);
+	if (sbuf == NULL) {
+		return 0;
+	}
 
 	data = acl_vstring_str(sbuf);
 	dlen = (int) ACL_VSTRING_LEN(sbuf);
@@ -266,10 +271,11 @@ static int chunked_data_endl(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 
 	ctx->body_len += dlen;
 
-	if (notify(HTTP_CHAT_CHUNK_DATA_ENDL, data, dlen, arg) < 0)
-		return (-1);
+	if (notify(HTTP_CHAT_CHUNK_DATA_ENDL, data, dlen, arg) < 0) {
+		return -1;
+	}
 	ctx->status = CHAT_S_CHUNK_HDR;
-	return (0);
+	return 0;
 }
 
 static int chunked_data(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
@@ -281,26 +287,28 @@ static int chunked_data(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 	int   dlen, ret;
 
 	if (ctx->chunked) {
-		ret = (int) HTTP_LEN_ROUND(ctx);
-		sbuf = acl_aio_readn_peek(astream, ret);
-	} else if (ctx->hdr->content_length <= 0)
 		sbuf = acl_aio_read_peek(astream);
-	else {
+	} else if (ctx->hdr->content_length <= 0) {
+		sbuf = acl_aio_read_peek(astream);
+	} else {
 		ret = (int) HTTP_LEN_ROUND(ctx);
 		if (ret <= 0) {
 			/* 说明本次 HTTP 数据已经读完且遇到对方关闭
 			 * 或对方发来了多余的数据，所以需要返回 -1
 			 */
 			DISABLE_READ(astream);
-			if (notify(HTTP_CHAT_OK, NULL, 0, arg) < 0)
+			if (notify(HTTP_CHAT_OK, NULL, 0, arg) < 0) {
 				return (-1);
-			return (-1);
-		} else
-			sbuf = acl_aio_readn_peek(astream, ret);
+			}
+			return -1;
+		} else {
+			sbuf = acl_aio_read_peek(astream);
+		}
 	}
 
-	if (sbuf == NULL)
-		return (0);
+	if (sbuf == NULL) {
+		return 0;
+	}
 
 	data = acl_vstring_str(sbuf);
 	dlen = (int) ACL_VSTRING_LEN(sbuf);
@@ -316,23 +324,26 @@ static int chunked_data(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 			 */
 			/* xxx: 禁止连续读 */
 			DISABLE_READ(astream);
-			if (notify(HTTP_CHAT_OK, data, dlen, arg) < 0)
-				return (-1);
-			return (1);
+			if (notify(HTTP_CHAT_OK, data, dlen, arg) < 0) {
+				return -1;
+			}
+			return 1;
 		}
 
 		/* 对于 chunk 传输，读完本数据块 */
-		if (notify(HTTP_CHAT_DATA, data, dlen, arg) < 0)
-			return (-1);
+		if (notify(HTTP_CHAT_DATA, data, dlen, arg) < 0) {
+			return -1;
+		}
 
 		/* 设置标志位开始读取块数据体的分隔行数据 */
 		ctx->status = CHAT_S_CHUNK_SEP;
-		return (0);
+		return 0;
 	}
 
-	if (notify(HTTP_CHAT_DATA, data, dlen, arg) < 0)
-		return (-1);
-	return (0);
+	if (notify(HTTP_CHAT_DATA, data, dlen, arg) < 0) {
+		return -1;
+	}
+	return 0;
 }
 
 static int chunked_trailer(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
@@ -344,8 +355,9 @@ static int chunked_trailer(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 	int   dlen;
 
 	sbuf = acl_aio_gets_peek(astream);
-	if (sbuf == NULL)
-		return (0);
+	if (sbuf == NULL) {
+		return 0;
+	}
 
 	data = acl_vstring_str(sbuf);
 	dlen = (int) ACL_VSTRING_LEN(sbuf);
@@ -355,13 +367,15 @@ static int chunked_trailer(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 
 	if (strcmp(data, "\r\n") == 0 || strcmp(data, "\n") == 0) {
 		DISABLE_READ(astream);
-		if ((dlen = notify(HTTP_CHAT_OK, data, dlen, arg)) < 0)
-			return (-1);
-		return (1);
+		if ((dlen = notify(HTTP_CHAT_OK, data, dlen, arg)) < 0) {
+			return -1;
+		}
+		return 1;
 	}
-	if (notify(HTTP_CHAT_CHUNK_TRAILER, data, dlen, arg) < 0)
-		return (-1);
-	return (0);
+	if (notify(HTTP_CHAT_CHUNK_TRAILER, data, dlen, arg) < 0) {
+		return -1;
+	}
+	return 0;
 }
 
 static int chunked_hdr(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
@@ -373,8 +387,9 @@ static int chunked_hdr(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 	int   dlen;
 
 	sbuf = acl_aio_gets_peek(astream);
-	if (sbuf == NULL)
-		return (0);
+	if (sbuf == NULL) {
+		return 0;
+	}
 
 	data = acl_vstring_str(sbuf);
 	dlen = (int) ACL_VSTRING_LEN(sbuf);
@@ -384,22 +399,25 @@ static int chunked_hdr(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 	ctx->read_cnt = 0;
 
 	ptr = strchr(data, ' ');
-	if (ptr)
+	if (ptr) {
 		*ptr = 0;
+	}
 	ctx->chunk_len = strtoul(data, NULL, 16);
-	if (ptr)
+	if (ptr) {
 		*ptr = ' ';
+	}
 
-	if (notify(HTTP_CHAT_CHUNK_HDR, data, dlen, arg) < 0)
-		return (-1);
+	if (notify(HTTP_CHAT_CHUNK_HDR, data, dlen, arg) < 0) {
+		return -1;
+	}
 
 	if (ctx->chunk_len == 0) {
 		ctx->status = CHAT_S_CHUNK_TAL;
-		return (0);
+		return 0;
 	}
 
 	ctx->status = CHAT_S_CHUNK_DAT;
-	return (0);
+	return 0;
 }
 
 static int body_can_read(ACL_ASTREAM *astream, void *context)
@@ -413,9 +431,10 @@ static int body_can_read(ACL_ASTREAM *astream, void *context)
 	while (1) {
 		if ((ret = acl_aio_can_read(astream)) == ACL_VSTREAM_EOF) {
 			(void) notify(HTTP_CHAT_ERR_IO, NULL, 0, arg);
-			return (-1);
-		} else if (ret == 0)
+			return -1;
+		} else if (ret == 0) {
 			break;
+		}
 
 		switch (ctx->status) {
 		case CHAT_S_CHUNK_HDR:
@@ -433,24 +452,26 @@ static int body_can_read(ACL_ASTREAM *astream, void *context)
 		default:
 			acl_msg_error("%s(%d): unknown status(%d)",
 				myname, __LINE__, ctx->status);
-			return (-1);
+			return -1;
 		}
-		if (ret < 0)
-			return (-1);
-		else if (ret == 1)
-			return (0);
+		if (ret < 0) {
+			return -1;
+		} else if (ret == 1) {
+			return 0;
+		}
 	}
 
 	acl_aio_enable_read(astream, body_can_read, ctx);
-	return (0);
+	return 0;
 }
 
 static void body_get(ACL_ASTREAM *astream, HTTP_CHAT_CTX *ctx)
 {
-	if (ctx->chunked)
+	if (ctx->chunked) {
 		ctx->status = CHAT_S_CHUNK_HDR;
-	else
+	} else {
 		ctx->status = CHAT_S_CHUNK_DAT;
+	}
 
 	acl_aio_enable_read(astream, body_can_read, ctx);
 }
@@ -462,10 +483,12 @@ void http_req_body_get_async(HTTP_REQ *request, ACL_ASTREAM *astream,
 	HTTP_CHAT_CTX *ctx;
 	HTTP_HDR *hdr;
 
-	if (request == NULL || astream == NULL || notify == NULL)
+	if (request == NULL || astream == NULL || notify == NULL) {
 		acl_msg_fatal("%s: input invalid", myname);
-	if (request->hdr_req == NULL)
+	}
+	if (request->hdr_req == NULL) {
 		acl_msg_fatal("%s: hdr null", myname);
+	}
 
 	hdr = &(request->hdr_req->hdr);
 	if (hdr->content_length <= 0) {
@@ -493,10 +516,12 @@ void http_res_body_get_async(HTTP_RES *respond, ACL_ASTREAM *astream,
 	HTTP_CHAT_CTX *ctx;
 	HTTP_HDR *hdr;
 
-	if (respond == NULL || astream == NULL || notify == NULL)
+	if (respond == NULL || astream == NULL || notify == NULL) {
 		acl_msg_fatal("%s, %s(%d): input invalid", myname, __FILE__, __LINE__);
-	if (respond->hdr_res == NULL)
+	}
+	if (respond->hdr_res == NULL) {
 		acl_msg_fatal("%s: hdr null", myname);
+	}
 
 	hdr = &(respond->hdr_res->hdr);
 	if (hdr->debug && hdr->content_length <= 0 && !hdr->chunked) {
