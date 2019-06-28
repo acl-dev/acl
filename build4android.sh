03ebd2614022c09a1fb6d64691853e17569cf0bf
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2019-06-28 10:54:13 +0800

    add build shell for android

---
+++ build4android.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## Below building parameters can be set YES or NO
+#ACL_CLIENT_ONLY
+#DISABLE_DB
+#HAS_MYSQL
+#HAS_MYSQL_DLL
+#HAS_PGSQL
+#HAS_PGSQL_DLL
+#HAS_SQLITE
+#HAS_SQLITE_DLL
+#HAS_POLARSSL
+#HAS_POLARSSL_DLL
+
+cmake -DCMAKE_TOOLCHAIN_FILE=/data/zsx/android-ndk-r20/build/cmake/android.toolchain.cmake \
+	-DANDROID_ABI="armeabi-v7a" \
+	-DANDROID_ARM_NEON=ON \
+	-DANDROID_PLATFORM=android-14 \
+	-DCMAKE_BUILD_TYPE=RELEASE \
+	-DCMAKE_SYSTEM_NAME=Android \
+	-DACL_CLIENT_ONLY=YES \
+	-DDISABLE_DB=YES \
+	-DHAS_POLARSSL_DLL=YES \
+	..
