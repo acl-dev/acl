6c9a37e0d299193ba0ab68e72c6a7dba7338281e
郑树新 <zhengshuxin@hotmail.com>
2019-08-06 10:26:19 +0800

    md5 sample can check one file's md5sum

--- lib_acl_cpp/samples/md5/main.cpp
+++ lib_acl_cpp/samples/md5/main.cpp
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include "md5_c.h"
 
-int main()
+static void base_test(void)
 {
 	const char* s = "中国人民银行！";
 	const char* key = "zsxxsz";
@@ -33,8 +33,7 @@ int main()
 
 	char ch;
 	size_t len = strlen(s);
-	for (size_t i = 0; i < len; i++)
-	{
+	for (size_t i = 0; i < len; i++) {
 		ch = s[i];
 		md5.update(&ch, 1);
 	}
@@ -52,8 +51,7 @@ int main()
 	len = 1024000;
 	char* buf = (char*) malloc(len);
 
-	for (size_t i = 0; i < len; i++)
-	{
+	for (size_t i = 0; i < len; i++) {
 		ch = i % 255;
 		buf[i] = ch;
 		md5.update(&ch, 1);
@@ -70,7 +68,51 @@ int main()
 		printf("OK2: %s\r\n", buf1);
 	else
 		printf("error, buf1: %s, buf2: %s\r\n", buf1, buf2);
-#ifdef WIN32
+}
+
+static void check_file(const char* filepath)
+{
+	char buf[33];
+	if (acl::md5::md5_file(filepath, NULL, 0, buf, sizeof(buf)) == -1) {
+		printf("md5 check %s error\r\n", filepath);
+	} else {
+		printf("md5 ok, file=%s, result=%s\r\n", filepath, buf);
+	}
+}
+
+static void usage(const char* procname)
+{
+	printf("usage: %s -h[help] -f filepath\r\n", procname);
+#if defined(_WIN32) || defined(_WIN64)
+	printf("Enter any key to continue...");
+	fflush(stdout);
+	getchar();
+
+#endif
+}
+int main(int argc, char* argv[])
+{
+	int ch;
+	acl::string filepath;
+
+	while ((ch = getopt(argc, argv, "hf:")) > 0) {
+		switch (ch) {
+		case 'h':
+			usage(argv[0]);
+			return 0;
+		case 'f':
+			filepath = optarg;
+			break;
+		default:
+			break;
+		}
+	}
+	base_test();
+	if (!filepath.empty()) {
+		check_file(filepath);
+	}
+
+#if defined(_WIN32) || defined(_WIN64)
 	getchar();
 #endif
 	return 0;
