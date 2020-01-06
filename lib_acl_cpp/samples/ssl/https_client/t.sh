045ff7a5da545852828ef440902cc94e7f05b28a
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2020-01-06 15:50:03 +0800

    test ssl demo

--- lib_acl_cpp/samples/ssl/https_client/t.sh
+++ lib_acl_cpp/samples/ssl/https_client/t.sh
@@ -8,9 +8,9 @@ if [ $os == 'Darwin' ]; then
 	echo ""
 	./https_client -f "../libmbedcrypto.dylib;../libmbedx509.dylib;../libmbedtls.dylib" -s echo.websocket.org:443 -S
 else
-	./https_client -f "../libmbedcrypto.dylib;../libmbedx509.dylib;../libmbedtls.so" -s www.sina.com.cn:443 -S
+	./https_client -f "../libmbedcrypto.so;../libmbedx509.so;../libmbedtls.so" -s www.sina.com.cn:443 -S
 	echo ""
-	./https_client -f "../libmbedcrypto.dylib;../libmbedx509.dylib;../libmbedtls.so" -s www.baidu.com:443 -S
+	./https_client -f "../libmbedcrypto.so;../libmbedx509.so;../libmbedtls.so" -s www.baidu.com:443 -S
 	echo ""
-	./https_client -f "../libmbedcrypto.dylib;../libmbedx509.dylib;../libmbedtls.so" -s echo.websocket.org:443 -S
+	./https_client -f "../libmbedcrypto.so;../libmbedx509.so;../libmbedtls.so" -s echo.websocket.org:443 -S
 fi
