5e6457bdd2d6745a6d2889e182b9908c7bcc6bc8
zhengshuxin <zsx@cloud-dev-test-dev012-bjdx11.qiyi.virtual>
2020-01-09 09:45:14 +0800

    update package version to 3.5.0-12

--- app/master/daemon/version.h
+++ app/master/daemon/version.h
@@ -2,7 +2,7 @@
 #define __MASTER_VERSION_INCLUDE_H__
 
 #define MASTER_NAME	"acl_master"
-#define MASTER_VERSION	"3.4.2-0"
-#define MASTER_DATE	"20190309"
+#define MASTER_VERSION	"3.4.2-12"
+#define MASTER_DATE	"20200109"
 
 #endif
