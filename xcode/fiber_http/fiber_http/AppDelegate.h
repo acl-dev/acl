2119035ec384606a0edfd3a28d3a3314dfb5ec0b
shuxin 　　zheng <zhengshuxin@gmail.com>
2020-04-20 15:02:46 +0800

    add test demos for xcode

---
+++ xcode/fiber_http/fiber_http/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fiber_http
+//
+//  Created by shuxin 　　zheng on 2020/4/19.
+//  Copyright © 2020 acl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcode/fiber_http/fiber_httpUITests/fiber_httpUITests.m
@@ -0,0 +1,38 @@
+//
+//  fiber_httpUITests.m
+//  fiber_httpUITests
+//
+//  Created by shuxin 　　zheng on 2020/4/19.
+//  Copyright © 2020 acl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fiber_httpUITests : XCTestCase
+
+@end
+
+@implementation fiber_httpUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
