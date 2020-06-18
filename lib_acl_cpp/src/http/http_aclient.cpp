1637b523238a944d0324bfa6e10d1260f185a702
shuxin 　　zheng <zhengshuxin@gmail.com>
2020-06-18 15:38:26 +0800

    enable ssl in default mode for http_aclient class

--- lib_acl_cpp/src/http/http_aclient.cpp
+++ lib_acl_cpp/src/http/http_aclient.cpp
@@ -42,12 +42,7 @@ http_aclient::http_aclient(aio_handle& handle, sslbase_conf* ssl_conf /* NULL */
 	header_ = NEW http_header;
 	memset(&ns_addr_, 0, sizeof(ns_addr_));
 	memset(&serv_addr_, 0, sizeof(serv_addr_));
-#if !defined(HAS_POLARSSL_DLL) && !defined(HAS_POLARSSL)
-    (void) ssl_conf_; // just avoiding compiling warning
-    ssl_enable_ = false;
-#else
-    ssl_enable_ = true;
-#endif
+	ssl_enable_ = true;
 }
 
 http_aclient::~http_aclient(void)
@@ -177,7 +172,6 @@ bool http_aclient::handle_connect(const ACL_ASTREAM_CTX *ctx)
 	// 注册 IO 超时回调处理对象
 	conn_->add_timeout_callback(this);
 
-#if defined(HAS_POLARSSL_DLL) || defined(HAS_POLARSSL)
 	if (!ssl_conf_ || !ssl_enable_) {
 		return this->on_connect();
 	}
@@ -198,9 +192,6 @@ bool http_aclient::handle_connect(const ACL_ASTREAM_CTX *ctx)
 	conn_->add_read_callback(this);
 	conn_->read_wait(rw_timeout_);
 	return true;
-#else
-	return this->on_connect();
-#endif
 }
 
 int http_aclient::connect_callback(const ACL_ASTREAM_CTX *ctx)
@@ -392,10 +383,8 @@ bool http_aclient::read_wakeup(void)
 	case HTTP_ACLIENT_STATUS_WS_READING:
 		acl_assert(ws_in_);
 		return handle_websocket();
-#if defined(HAS_POLARSSL_DLL) || defined(HAS_POLARSSL)
 	case HTTP_ACLIENT_STATUS_SSL_HANDSHAKE:
 		return handle_ssl_handshake();
-#endif
 	default:
 		logger_error("invalid status=%u", status_);
 		return false;
@@ -404,7 +393,6 @@ bool http_aclient::read_wakeup(void)
 
 bool http_aclient::handle_ssl_handshake(void)
 {
-#if defined(HAS_POLARSSL_DLL) || defined(HAS_POLARSSL)
 	// 否则，则是第一次进行 SSL 握手阶段的 IO 过程
 	sslbase_io* ssl_io = (sslbase_io*) conn_->get_hook();
 	if (ssl_io == NULL) {
@@ -426,10 +414,6 @@ bool http_aclient::handle_ssl_handshake(void)
 
 	// 继续 SSL 握手过程
 	return true;
-#else
-	logger_error("shouldn't come here in no SSL mode!");
-	return false;
-#endif
 }
 
 bool http_aclient::read_callback(char* data, int len)
