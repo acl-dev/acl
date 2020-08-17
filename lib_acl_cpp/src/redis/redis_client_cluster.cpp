bb2aa454242f821ed0d537c965add67467c8bc2a
shuxin 　　zheng <zhengshuxin@gmail.com>
2020-08-17 12:00:27 +0800

    when one redis node wasn't set, then using the first redis node's config.

--- lib_acl_cpp/src/redis/redis_client_cluster.cpp
+++ lib_acl_cpp/src/redis/redis_client_cluster.cpp
@@ -373,6 +373,8 @@ redis_client* redis_client_cluster::move(redis_command& cmd,
 		return NULL;
 	}
 
+	// 从连接池集合中提取目标redis节点的配置项，如果不存在，则默认使用
+	// 所设置的第一个redis节点的配置项
 	const conn_config* conf = this->get_config(addr, true);
 	if (conf == NULL) {
 		logger_error("no conn_config for addr=%s", addr);
@@ -413,7 +415,9 @@ redis_client* redis_client_cluster::ask(redis_command& cmd,
 		return NULL;
 	}
 
-	const conn_config* conf = this->get_config(addr);
+	// 从连接池集合中提取目标redis节点的配置项，如果不存在，则默认使用
+	// 所设置的第一个redis节点的配置项
+	const conn_config* conf = this->get_config(addr, true);
 	if (conf == NULL) {
 		logger_error("no conn_config for addr=%s", addr);
 		return NULL;
