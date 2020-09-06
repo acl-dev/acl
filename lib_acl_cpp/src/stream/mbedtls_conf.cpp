42e0249ff5e7fefacde467cf4e91250bcd539d0c
shuxin 　　zheng <zhengshuxin@gmail.com>
2020-09-06 22:34:29 +0800

    merge mbedtls_conf

--- lib_acl_cpp/src/stream/mbedtls_conf.cpp
+++ lib_acl_cpp/src/stream/mbedtls_conf.cpp
@@ -62,6 +62,7 @@
 #  define SSL_LIST_CIPHERSUITES_NAME	"mbedtls_ssl_list_ciphersuites"
 
 #  define SSL_CONF_INIT_NAME		"mbedtls_ssl_config_init"
+#  define SSL_CONF_FREE_NAME		"mbedtls_ssl_config_free"
 #  define SSL_CONF_DEFAULTS_NAME	"mbedtls_ssl_config_defaults"
 #  define SSL_CONF_RNG_NAME		"mbedtls_ssl_conf_rng"
 #  define SSL_CONF_ENDPOINT_NAME	"mbedtls_ssl_conf_endpoint"
@@ -70,7 +71,6 @@
 #  define SSL_CONF_CA_CHAIN_NAME	"mbedtls_ssl_conf_ca_chain"
 #  define SSL_CONF_OWN_CERT_NAME	"mbedtls_ssl_conf_own_cert"
 #  define SSL_CONF_AUTHMODE_NAME	"mbedtls_ssl_conf_authmode"
-#  define SSL_CONF_INIT_FREE		"mbedtls_ssl_config_free"
 #  ifdef DEBUG_SSL
 #   define SSL_CONF_DBG_NAME		"mbedtls_ssl_conf_dbg"
 #  endif
@@ -118,6 +118,7 @@ typedef int  (*x509_crt_parse_file_fn)(X509_CRT*, const char*);
 typedef const int* (*ssl_list_ciphersuites_fn)(void);
 
 typedef void (*ssl_config_init_fn)(mbedtls_ssl_config*);
+typedef void (*ssl_config_free_fn)(mbedtls_ssl_config*);
 typedef void (*ssl_conf_rng_fn)(mbedtls_ssl_config*,
 		int (*f_rng)(void*, unsigned char*, size_t), void *p_rng);
 typedef void (*ssl_conf_endpoint_fn)(mbedtls_ssl_config*, int);
@@ -171,6 +172,7 @@ static x509_crt_parse_file_fn		__x509_crt_parse_file;
 static ssl_list_ciphersuites_fn		__ssl_list_ciphersuites;
 
 static ssl_config_init_fn		__ssl_config_init;
+static ssl_config_free_fn		__ssl_config_free;
 static ssl_config_defaults_fn		__ssl_config_defaults;
 static ssl_conf_rng_fn			__ssl_conf_rng;
 static ssl_conf_endpoint_fn		__ssl_conf_endpoint;
@@ -310,6 +312,7 @@ static bool load_from_ssl(void)
 	LOAD_SSL(SSL_LIST_CIPHERSUITES_NAME, ssl_list_ciphersuites_fn, __ssl_list_ciphersuites);
 
 	LOAD_SSL(SSL_CONF_INIT_NAME, ssl_config_init_fn, __ssl_config_init);
+	LOAD_SSL(SSL_CONF_FREE_NAME, ssl_config_free_fn, __ssl_config_free);
 	LOAD_SSL(SSL_CONF_DEFAULTS_NAME, ssl_config_defaults_fn, __ssl_config_defaults);
 	LOAD_SSL(SSL_CONF_RNG_NAME, ssl_conf_rng_fn, __ssl_conf_rng);
 	LOAD_SSL(SSL_CONF_ENDPOINT_NAME, ssl_conf_endpoint_fn, __ssl_conf_endpoint);
@@ -318,7 +321,6 @@ static bool load_from_ssl(void)
 	LOAD_SSL(SSL_CONF_CA_CHAIN_NAME, ssl_conf_ca_chain_fn, __ssl_conf_ca_chain);
 	LOAD_SSL(SSL_CONF_OWN_CERT_NAME, ssl_conf_own_cert_fn, __ssl_conf_own_cert);
 	LOAD_SSL(SSL_CONF_AUTHMODE_NAME, ssl_conf_authmode_fn, __ssl_conf_authmode);
-	LOAD_SSL(SSL_CONF_INIT_FREE, ssl_config_init_fn, __ssl_config_free);
 # ifdef DEBUG_SSL
 	LOAD_SSL(SSL_CONF_DBG_NAME, ssl_conf_dbg_fn, __ssl_conf_dbg);
 # endif
@@ -442,6 +444,7 @@ static void mbedtls_dll_load(void)
 
 #  define __ssl_list_ciphersuites	::mbedtls_ssl_list_ciphersuites
 #  define __ssl_config_init		::mbedtls_ssl_config_init
+#  define __ssl_config_free		::mbedtls_ssl_config_free
 #  define __ssl_config_defaults		::mbedtls_ssl_config_defaults
 #  define __ssl_conf_rng		::mbedtls_ssl_conf_rng
 #  define __ssl_conf_endpoint		::mbedtls_ssl_conf_endpoint
@@ -450,7 +453,6 @@ static void mbedtls_dll_load(void)
 #  define __ssl_conf_ca_chain		::mbedtls_ssl_conf_ca_chain
 #  define __ssl_conf_own_cert		::mbedtls_ssl_conf_own_cert
 #  define __ssl_conf_authmode		::mbedtls_ssl_conf_authmode
-#  define __ssl_config_free		::mbedtls_ssl_config_free
 #  ifdef DEBUG_SSL
 #   define __ssl_conf_dbg		::mbedtls_ssl_conf_dbg
 #  endif
@@ -718,7 +720,7 @@ mbedtls_conf::~mbedtls_conf(void)
 		__entropy_free((mbedtls_entropy_context*) entropy_);
 	}
 
-	__ssl_config_free((mbedtls_ssl_config*)conf_);
+	__ssl_config_free((mbedtls_ssl_config*) conf_);
 	acl_myfree(conf_);
 	acl_myfree(entropy_);
 
@@ -800,7 +802,8 @@ bool mbedtls_conf::load_ca(const char* ca_file, const char* ca_path)
 #endif
 }
 
-bool mbedtls_conf::append_key_cert(const char* crt_file, const char* key_file, const char* key_pass)
+bool mbedtls_conf::append_key_cert(const char* crt_file, const char* key_file,
+	const char* key_pass)
 {
 	if (crt_file == NULL || crt_file[0] == '\0' ||
 		key_file == NULL || key_file[0] == '\0') {
@@ -812,6 +815,7 @@ bool mbedtls_conf::append_key_cert(const char* crt_file, const char* key_file, c
 	int ret = 0;
 	X509_CRT *cert = NULL;
 	PKEY *pkey = NULL;
+
 	if (!init_once()) {
 		logger_error("init_once error");
 		return false;
@@ -841,7 +845,8 @@ bool mbedtls_conf::append_key_cert(const char* crt_file, const char* key_file, c
 	cert_status_ = CONF_OWN_CERT_OK;
 	return true;
 ERR:
-	logger_error("append_key_cert(%s:%s) error: -0x%04x", crt_file, key_file, -ret);
+	logger_error("append_key_cert(%s:%s) error: -0x%04x",
+		crt_file, key_file, -ret);
 	if (cert) {
 		__x509_crt_free(cert);
 		mbedtls_free(cert);
