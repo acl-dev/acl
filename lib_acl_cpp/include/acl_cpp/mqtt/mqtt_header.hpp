9c3cfce41658ecac3f668fac9ab1781e8acc38f7
shuxin 　　zheng <zhengshuxin@gmail.com>
2021-03-01 18:27:11 +0800

    writing CONNECT of mqtt module

--- lib_acl_cpp/include/acl_cpp/mqtt/mqtt_header.hpp
+++ lib_acl_cpp/include/acl_cpp/mqtt/mqtt_header.hpp
@@ -24,6 +24,20 @@ typedef enum {
 } mqtt_type_t;
 
 typedef enum {
+	MQTT_STAT_HDR_TYPE	= 0,
+	MQTT_STAT_HDR_LEN	= 1,
+	MQTT_STAT_HDR_VAR	= 2,
+	MQTT_STAT_STR_LEN	= 4,
+	MQTT_STAT_STR_VAL	= 5,
+
+	MQTT_STAT_CID		= 6,
+	MQTT_STAT_USERNAME	= 7,
+	MQTT_STAT_PASSWD	= 8,
+	MQTT_STAT_WILL_TOPIC	= 9,
+	MQTT_STAT_WILL_MSG	= 9,
+} mqtt_status_t;
+
+typedef enum {
 	MQTT_NONE,
 	MQTT_NEED,
 	MQTT_MAYBE,
@@ -51,14 +65,49 @@ public:
 	virtual ~mqtt_message(void);
 
 protected:
-	mqtt_type_t type_;
-	unsigned length_;
+	mqtt_status_t status_;
 
-	void set_payload_length(unsigned len);
+	mqtt_type_t type_;
+
+	void set_data_length(unsigned len);
 	bool pack_header(string& out);
 	void pack_add(unsigned char ch, string& out);
 	void pack_add(unsigned short n, string& out);
 	void pack_add(const string& s, string& out);
+
+	bool fix_header_finish(void) const {
+		return status_ != MQTT_STAT_HDR_TYPE
+			&& status_ != MQTT_STAT_HDR_LEN;
+	}
+
+	unsigned get_data_length(void) const {
+		return dlen_;
+	}
+
+	int unpack_header(const char* data, unsigned dlen);
+
+	void unpack_string_await(string& buf, mqtt_status_t next);
+
+	bool unpack_char(const char* data, size_t len, unsigned char& out);
+	bool unpack_short(const char* data, size_t len, unsigned short& out);
+
+private:
+	unsigned char hflags_:4;
+
+	char hbuf_[4];
+	unsigned hlen_;
+	unsigned dlen_;
+
+	mqtt_status_t next_;
+	string* buff_;
+	unsigned short vlen_;
+
+public:
+	int unpack_string_len(const char* data, unsigned dlen);
+	int unpack_string_val(const char* data, unsigned dlen);
+
+	int unpack_header_type(const char* data, unsigned dlen);
+	int unpack_header_len(const char* data, unsigned dlen);
 };
 
 typedef enum {
@@ -90,8 +139,44 @@ public:
 
 	bool to_string(string& out);
 
+	int update(const char* data, unsigned dlen);
+
+	bool is_finished(void) const {
+		return finished_;
+	}
+
+	unsigned short get_keep_alive(void) const {
+		return keep_alive_;
+	}
+
+	const char* get_username(void) const {
+		return username_.empty() ? NULL : username_.c_str();
+	}
+
+	const char* get_passwd(void) const {
+		return passwd_.empty() ? NULL : passwd_.c_str();
+	}
+
+	mqtt_qos_t get_qos(void) const {
+		return qos_;
+	}
+
+	const char* get_will_topic(void) const {
+		return will_topic_.empty() ? NULL : will_topic_.c_str();
+	}
+
+	const char* get_will_msg(void) const {
+		return will_msg_.empty() ? NULL : will_msg_.c_str();
+	}
+
 private:
-	unsigned char flags_;
+	char hbuf_[10];
+	unsigned hlen_;
+
+	bool finished_;
+
+	mqtt_qos_t qos_;
+	unsigned char conn_flags_;
 	unsigned short keep_alive_;
 
 	string cid_;
@@ -99,6 +184,20 @@ private:
 	string passwd_;
 	string will_topic_;
 	string will_msg_;
+
+public:
+	int unpack_header_var(const char* data, unsigned dlen);
+	int unpack_cid(const char* data, unsigned dlen);
+	int unpack_username(const char* data, unsigned dlen);
+	int unpack_passwd(const char* data, unsigned dlen);
+	int unpack_will_topic(const char* data, unsigned dlen);
+	int unpack_will_msg(const char* data, unsigned dlen);
+};
+
+class mqtt_connack : public mqtt_message {
+public:
+	mqtt_connack(void);
+	~mqtt_connack(void);
 };
 
 } // namespace acl
--- lib_acl_cpp/src/mqtt/mqtt_header.cpp
+++ lib_acl_cpp/src/mqtt/mqtt_header.cpp
@@ -30,24 +30,30 @@ const char* mqtt_type_desc(mqtt_type_t type) {
 }
 
 mqtt_message::mqtt_message(mqtt_type_t type)
-: type_(type)
+: status_(MQTT_STAT_HDR_TYPE)
+, type_(type)
+, hflags_(0)
+, dlen_(0)
 {
 	assert(type > MQTT_CONNACK && type < MQTT_RESERVED_MAX);
 	if (__constrains[type].id == MQTT_NEED) {
-		length_ = 2;
+		hlen_ = 2;
+	} else {
+		hlen_ = 0;
 	}
 }
 
 mqtt_message::~mqtt_message(void) {}
 
-void mqtt_message::set_payload_length(unsigned len) {
+void mqtt_message::set_data_length(unsigned len) {
 	if (len == 0) {
 		return;
 	}
 
 	if (__constrains[type_].payload != MQTT_NEED) {
 		logger_error("%s: needn't payload", mqtt_type_desc(type_));
-		return;
+	} else {
+		dlen_ = len;
 	}
 }
 
@@ -57,25 +63,25 @@ bool mqtt_message::pack_header(string& out) {
 
 	memset(header, 0, sizeof(header));
 
-	header[0] = (((char) type_) << 4) & 0xff;
+	header[0] = ((((char) type_) << 4) & 0xff) | hflags_;
 	len++;
 
-	if (length_ <= 127) {
-		header[len++] = length_ & 0xef;
-	} else if (length_ <= 16383) {
-		header[len++] = ((unsigned char) (length_ >> 8) & 0xff) | 127;
-		header[len++] = (unsigned char) length_ & 0xef;
-	} else if (length_ <= 2097151) {
-		header[len++] = ((unsigned char)(length_ >> 16) & 0xff) | 127;
-		header[len++] = ((unsigned char)(length_ >> 8 ) & 0xff) | 127;
-		header[len++] = (unsigned char) length_ & 0xef;
-	} else if (length_ <= 268435455) {
-		header[len++] = ((unsigned char)(length_ >> 24) & 0xff) | 127;
-		header[len++] = ((unsigned char)(length_ >> 16) & 0xff) | 127;
-		header[len++] = ((unsigned char)(length_ >> 8 ) & 0xff) | 127;
-		header[len++] = (unsigned char) length_ & 0xef;
+	if (dlen_ <= 127) {
+		header[len++] = dlen_ & 0x7f;
+	} else if (dlen_ <= 16383) {
+		header[len++] = ((unsigned char) (dlen_ >> 8) & 0x7f) | 0x80;
+		header[len++] = (unsigned char) dlen_ & 0x7f;
+	} else if (dlen_ <= 2097151) {
+		header[len++] = ((unsigned char)(dlen_ >> 16) & 0x7f) | 0x80;
+		header[len++] = ((unsigned char)(dlen_ >> 8 ) & 0x7f) | 0x80;
+		header[len++] = (unsigned char)  dlen_ & 0x7f;
+	} else if (dlen_ <= 268435455) {
+		header[len++] = ((unsigned char)(dlen_ >> 24) & 0x7f) | 0x80;
+		header[len++] = ((unsigned char)(dlen_ >> 16) & 0x7f) | 0x80;
+		header[len++] = ((unsigned char)(dlen_ >> 8 ) & 0x7f) | 0x80;
+		header[len++] = (unsigned char)  dlen_ & 0x7f;
 	} else {
-		logger_error("invalid length_=%u", length_);
+		logger_error("invalid dlen_=%u", dlen_);
 		return false;
 	}
 
@@ -101,9 +107,173 @@ void mqtt_message::pack_add(const string& s, string& out) {
 	}
 }
 
+bool mqtt_message::unpack_char(const char* in, size_t len, unsigned char& out) {
+	if (len < 1) {
+		logger_error("too short: %ld", (long) len);
+		return false;
+	}
+	out = (unsigned char) in[0];
+	return true;
+}
+
+bool mqtt_message::unpack_short(const char* in, size_t len, unsigned short& out) {
+	if (len < 2) {
+		logger_error("too short: %ld", (long) len);
+		return false;
+	}
+
+	out = (unsigned short) (((unsigned short) (in[0] & 0xff) << 8)
+			| (unsigned short) (in[1] & 0xff));
+	return true;
+}
+
+int mqtt_message::unpack_header_type(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	char ch = *data++;
+	char type = (ch &0xff) >> 4;
+	if (type <= MQTT_RESERVED_MIN || type >= MQTT_RESERVED_MAX) {
+		logger_error("invalid type=%d", type);
+		return -1;
+	}
+
+	type_   = (mqtt_type_t) type;
+	hflags_ = ch & 0x0f;
+	status_ = MQTT_STAT_HDR_LEN;
+	hlen_   = 0;
+	return --dlen;
+}
+
+int mqtt_message::unpack_header_len(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+	if (hlen_ >= sizeof(hbuf_)) {
+		logger_error("invalid header len");
+		return -1;
+	}
+
+	for (; hlen_ < sizeof(hbuf_) && dlen > 0;) {
+		hbuf_[hlen_] = *data++;
+		dlen--;
+
+		if ((hbuf_[hlen_++] & 0x80) == 0) {
+			break;
+		}
+	}
+
+	assert(hlen_ > 0 && hlen_ <= sizeof(hbuf_));
+	if ((hbuf_[hlen_ - 1] & 0x80) != 0) {
+		if (hlen_ == 4) {
+			logger_error("invalid header len");
+			return -1;
+		}
+		assert(dlen == 0);
+		return dlen;
+	}
+
+	status_ = MQTT_STAT_HDR_VAR;
+
+#if 1
+	dlen_ = 0;
+	for (unsigned i = 0; i < hlen_; i++) {
+		dlen_ |= (unsigned) ((hbuf_[i] & 0x7f) << (8 * i));
+	}
+#else
+	if (hlen_ == 1) {
+		dlen_ = (unsigned) (hbuf_[0] & 0x7f);
+	} else if (hlen_ == 2) {
+		dlen_ = (unsigned) (((hbuf_[0] & 0x7f) << 8)
+				| (hbuf_[1] & 0x7f));
+	} else if (hlen_ == 3) {
+		dlen_ = (unsigned) (((hbuf_[0] & 0x7f) << 16)
+				| ((hbuf_[1] & 0x7f) << 8)
+				| (hbuf_[0] & 0x7f));
+	} else if (hlen_ == 4) {
+		dlen_ = (unsigned) (((hbuf_[0] & 0x7f) << 24)
+				| ((hbuf_[1] & 0x7f) << 16)
+				| ((hbuf_[2] & 0x7f) << 8)
+				| (hbuf_[0] & 0x7f));
+	}
+#endif
+
+	return dlen;
+}
+
+int mqtt_message::unpack_header(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		logger_error("input invalid");
+		return -1;
+	}
+	switch (status_) {
+	case MQTT_STAT_HDR_TYPE:
+		return unpack_header_type(data, dlen);
+	case MQTT_STAT_HDR_LEN:
+		return unpack_header_len(data, dlen);
+	default:
+		return dlen;
+	}
+}
+
+void mqtt_message::unpack_string_await(string& out, mqtt_status_t next) {
+	out.clear();
+	buff_   = &out;
+	next_   = next;
+	hlen_   = 0;
+	status_ = MQTT_STAT_STR_LEN;
+}
+
+#define	HDR_LEN_LEN	2
+
+int mqtt_message::unpack_string_len(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	for (; hlen_ < HDR_LEN_LEN && dlen > 0;) {
+		hbuf_[hlen_++] = *data++;
+		dlen--;
+	}
+
+	if (hlen_ < HDR_LEN_LEN) {
+		assert(dlen == 0);
+		return dlen;
+	}
+
+	if (!unpack_short(&hbuf_[0], 2, vlen_)) {
+		logger_error("unpack cid len error");
+		return -1;
+	}
+
+	status_ = MQTT_STAT_STR_VAL;
+	return dlen;
+}
+
+int mqtt_message::unpack_string_val(const char* data, unsigned dlen) {
+	assert(buff_->size() <= (size_t) vlen_);
+
+	size_t i, left = (size_t) vlen_ - buff_->size();
+	for (i = 0; i < left && dlen > 0; i++) {
+		(*buff_) += *data++;
+		dlen--;
+	}
+
+	if (buff_->size() == (size_t) vlen_) {
+		status_ = next_;
+		return dlen;
+	}
+	assert(dlen == 0);
+	return dlen;
+}
+
 mqtt_connect::mqtt_connect(void)
 : mqtt_message(MQTT_CONNACK)
-, flags_(0)
+, hlen_(0)
+, finished_(false)
+, qos_(MQTT_QOS0)
+, conn_flags_(0)
 , keep_alive_(300)
 {
 }
@@ -117,29 +287,31 @@ void mqtt_connect::set_keep_alive(unsigned short keep_alive) {
 void mqtt_connect::set_username(const char* name) {
 	if (name && *name) {
 		username_ = name;
-		flags_ |= 1 << 7;
+		conn_flags_ |= 1 << 7;
 	}
 }
 
 void mqtt_connect::set_passwd(const char* passwd) {
 	if (passwd && *passwd) {
 		passwd_ = passwd;
-		flags_ |= 1 << 6;
+		conn_flags_ |= 1 << 6;
 	}
 }
 
 void mqtt_connect::set_qos(mqtt_qos_t qos) {
+	qos_ = qos;
+
 	switch (qos) {
 	case MQTT_QOS0:
-		flags_ &= ~(1 << 4 | 1 << 3);
+		conn_flags_ &= ~(1 << 4 | 1 << 3);
 		break;
 	case MQTT_QOS1:
-		flags_ &= ~(1 << 4);
-		flags_ |= 1 << 3;
+		conn_flags_ &= ~(1 << 4);
+		conn_flags_ |= 1 << 3;
 		break;
 	case MQTT_QOS2:
-		flags_ &= ~(1 << 3);
-		flags_ |= 1 << 4;
+		conn_flags_ &= ~(1 << 3);
+		conn_flags_ |= 1 << 4;
 		break;
 	default:
 		logger_error("invalid qos: %d", (int) qos);
@@ -174,7 +346,7 @@ bool mqtt_connect::to_string(string& out) {
 		len += (unsigned) passwd_.size();
 	}
 	if (!will_topic_.empty() && !will_msg_.empty()) {
-		flags_ |= 1 << 2;
+		conn_flags_ |= 1 << 2;
 
 		len += 2;
 		len += (unsigned) will_topic_.size();
@@ -186,7 +358,7 @@ bool mqtt_connect::to_string(string& out) {
 		will_msg_.clear();
 	}
 
-	this->set_payload_length(len);
+	this->set_data_length(len);
 	if (!this->pack_header(out)) {
 		out.set_bin(old_mode);
 		return false;
@@ -199,7 +371,7 @@ bool mqtt_connect::to_string(string& out) {
 	this->pack_add((unsigned char) 'T', out);
 	this->pack_add((unsigned char) 'T', out);
 	this->pack_add((unsigned char) 0x04, out);
-	this->pack_add((unsigned char) flags_, out);
+	this->pack_add((unsigned char) conn_flags_, out);
 	this->pack_add((unsigned short) keep_alive_, out);
 
 	this->pack_add(cid_, out);
@@ -219,6 +391,161 @@ bool mqtt_connect::to_string(string& out) {
 
 	out.set_bin(old_mode);
 	return true;
+}
+
+static struct {
+	mqtt_status_t status;
+	int (mqtt_connect::*handler)(const char*, unsigned);
+} handlers[] = {
+	{ MQTT_STAT_HDR_TYPE,	&mqtt_connect::unpack_header_type	},
+	{ MQTT_STAT_HDR_LEN,	&mqtt_connect::unpack_header_len	},
+	{ MQTT_STAT_HDR_VAR,	&mqtt_connect::unpack_header_var	},
+	{ MQTT_STAT_STR_LEN,	&mqtt_connect::unpack_string_len	},
+	{ MQTT_STAT_STR_VAL,	&mqtt_connect::unpack_string_val	},
+	{ MQTT_STAT_CID,	&mqtt_connect::unpack_cid		},
+	{ MQTT_STAT_USERNAME,	&mqtt_connect::unpack_username		},
+	{ MQTT_STAT_PASSWD,	&mqtt_connect::unpack_passwd		},
+	{ MQTT_STAT_WILL_TOPIC,	&mqtt_connect::unpack_will_topic	},
+	{ MQTT_STAT_WILL_MSG,	&mqtt_connect::unpack_will_msg		},
+};
+
+int mqtt_connect::update(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		logger_error("invalid input");
+		return -1;
+	}
+
+	while (dlen > 0 && !finished_) {
+		int ret = (this->*handlers[status_].handler)(data, dlen);
+		if (ret < 0) {
+			return -1;
+		}
+		dlen = (unsigned) ret;
+		data += ret;
+	}
+	return dlen;
+}
+
+#define	HDR_VAR_LEN	10
+
+int mqtt_connect::unpack_header_var(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+	assert(sizeof(hbuf_) >= HDR_VAR_LEN);
+
+	if (hlen_ >= HDR_VAR_LEN) {
+		logger_error("invalid header var");
+		return -1;
+	}
+
+	for (; hlen_ < HDR_VAR_LEN && dlen > 0;) {
+		hbuf_[hlen_++] = *data++;
+		dlen--;
+	}
+
+	if (hlen_ < HDR_VAR_LEN) {
+		assert(dlen == 0);
+		return dlen;
+	}
+
+	char label[5];
+	label[0] = hbuf_[2];
+	label[1] = hbuf_[3];
+	label[2] = hbuf_[4];
+	label[3] = hbuf_[5];
+	label[4] = 0;
+	if (strcasecmp(label, "MQTT") != 0) {
+		logger_warn("invalid label: %s", label);
+	}
+	conn_flags_ = hbuf_[7];
+	if (!this->unpack_short(&hbuf_[9], 2, keep_alive_)) {
+		logger_error("unpack keep_alive error");
+		return -1;
+	}
+
+	status_ = MQTT_STAT_CID;
+	hlen_   = 0;
+	return dlen;
+}
+
+int mqtt_connect::unpack_cid(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	mqtt_status_t next;
+	if ((conn_flags_ & 0x80)) {
+		next = MQTT_STAT_USERNAME;
+	} else if ((conn_flags_ & 0x40)) {
+		next = MQTT_STAT_PASSWD;
+	} else if ((conn_flags_ & 0x04)) {
+		next = MQTT_STAT_WILL_TOPIC;
+	} else {
+		finished_ = true;
+		return dlen;
+	}
+
+	this->unpack_string_await(cid_, next);
+	return dlen;
+}
+
+int mqtt_connect::unpack_username(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	mqtt_status_t next;
+	if ((conn_flags_ & 0x40)) {
+		next = MQTT_STAT_PASSWD;
+	} else if ((conn_flags_ & 0x04)) {
+		next = MQTT_STAT_WILL_TOPIC;
+	} else {
+		finished_ = true;
+		return dlen;
+	}
+
+	this->unpack_string_await(username_, next);
+	return dlen;
+}
+
+int mqtt_connect::unpack_passwd(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	mqtt_status_t next;
+	if ((conn_flags_ & 0x04)) {
+		next = MQTT_STAT_WILL_TOPIC;
+	} else {
+		finished_ = true;
+		return dlen;
+	}
+
+	this->unpack_string_await(passwd_, next);
+	return dlen;
+}
+
+int mqtt_connect::unpack_will_topic(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	mqtt_status_t next = MQTT_STAT_WILL_MSG;
+
+	this->unpack_string_await(will_topic_, next);
+	return dlen;
+}
+
+int mqtt_connect::unpack_will_msg(const char* data, unsigned dlen) {
+	if (data == NULL || dlen == 0) {
+		return 0;
+	}
+
+	mqtt_status_t next = MQTT_STAT_WILL_MSG;
+
+	this->unpack_string_await(will_msg_, next);
+	return dlen;
 }
 
 } //namespace acl
