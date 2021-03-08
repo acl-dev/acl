4e47c898ca155a81346b95ef29ad81fc02ef6827
shuxin 　　zheng <zhengshuxin@gmail.com>
2021-03-08 16:21:51 +0800

    test mqtt module

--- lib_acl_cpp/src/mqtt/mqtt_pubrel.cpp
+++ lib_acl_cpp/src/mqtt/mqtt_pubrel.cpp
@@ -8,11 +8,13 @@ namespace acl {
 mqtt_pubrel::mqtt_pubrel(void)
 : mqtt_ack(MQTT_PUBREL)
 {
+	this->get_header().set_header_flags(0x02);
 }
 
 mqtt_pubrel::mqtt_pubrel(const mqtt_header& header)
 : mqtt_ack(header)
 {
+	this->get_header().set_header_flags(0x02);
 }
 
 mqtt_pubrel::~mqtt_pubrel(void) {}
