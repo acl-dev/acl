12d402a69ca1dced4998a26df425260f217197da
郑树新 <zhengshuxin@hotmail.com>
2022-05-02 21:57:47 +0800

    build dns-gate on windows

--- app/wizard_demo/dns-gate/service/manage/http_servlet.cpp
+++ app/wizard_demo/dns-gate/service/manage/http_servlet.cpp
@@ -79,7 +79,6 @@ bool http_servlet::doOther(HttpRequest&, HttpResponse& res, const char* method)
 {
 	res.setStatus(400);
 	res.setContentType("text/xml; charset=utf-8");
-	// 发送 http 响应体
 	acl::string buf;
 	buf.format("<root error='unkown method %s' />\r\n", method);
 	res.write(buf);
@@ -89,9 +88,6 @@ bool http_servlet::doOther(HttpRequest&, HttpResponse& res, const char* method)
 
 bool http_servlet::doService(int type, HttpRequest& req, HttpResponse& res)
 {
-	// 如果需要 http session 控制，请打开下面注释，且需要保证
-	// 在 master_service.cpp 的函数 thread_on_read 中设置的
-	// memcached 或 redis 服务正常
 	/*
 	const char* sid = req.getSession().getAttribute("sid");
 	if (*sid == 0) {
