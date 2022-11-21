638b966ec6b627ee2759a6688c92b9c6768b01df
zhengshuxin <zhengshuxin@gmail.com>
2022-11-21 20:44:59 +0800

    add header

--- lib_fiber/cpp/include/fiber/libfiber.hpp
+++ lib_fiber/cpp/include/fiber/libfiber.hpp
@@ -5,12 +5,12 @@
 #include "fiber_lock.hpp"
 #include "fiber_event.hpp"
 #include "fiber_cond.hpp"
-#include "fiber_tbox.hpp"
 #include "wait_group.hpp"
 #include "fiber_sem.hpp"
 #include "channel.hpp"
 
 #if defined(ACL_CPP_API)
+# include "fiber_tbox.hpp"
 # include "master_fiber.hpp"
 # include "fiber_redis_pipeline.hpp"
 # if !defined(_WIN32) && !defined(_WIN64)
