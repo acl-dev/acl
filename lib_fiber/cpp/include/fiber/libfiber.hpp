6edfd3c74d2cb6c451fdfe11ab44f919b1b909b3
zhengshuxin <zsx@zhengshuxin210-1322324fc.qiyi.virtual>
2023-02-04 20:49:54 +0800

    headers sequence

--- lib_fiber/cpp/include/fiber/libfiber.hpp
+++ lib_fiber/cpp/include/fiber/libfiber.hpp
@@ -7,12 +7,12 @@
 #include "fiber_mutex.hpp"
 #include "fiber_mutex_stat.hpp"
 #include "fiber_cond.hpp"
+#include "fiber_tbox.hpp"
 #include "wait_group.hpp"
 #include "fiber_sem.hpp"
 #include "channel.hpp"
 
 #if defined(ACL_CPP_API)
-# include "fiber_tbox.hpp"
 # include "master_fiber.hpp"
 # include "fiber_redis_pipeline.hpp"
 # if !defined(_WIN32) && !defined(_WIN64)
