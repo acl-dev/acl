540650660e6cb1ac1cc8220f63fcdf43026e281f
zhengshuxin <zhengshuxin@gmail.com>
2023-02-12 14:03:00 +0800

    Add openssl supporting in demo.

--- lib_acl_cpp/samples/ssl/server/main.cpp
+++ lib_acl_cpp/samples/ssl/server/main.cpp
@@ -43,6 +43,7 @@ private:
 			return false;
 		}
 
+		printf("ssl handshake ok!\r\n");
 		return true;
 	}
 
@@ -81,7 +82,7 @@ static void start_server(const acl::string addr, acl::sslbase_conf& ssl_conf) {
 }
 
 static bool ssl_init(const acl::string& ssl_crt, const acl::string& ssl_key,
-	acl::mbedtls_conf& ssl_conf) {
+	acl::sslbase_conf& ssl_conf) {
 
 	ssl_conf.enable_cache(true);
 
@@ -96,30 +97,22 @@ static bool ssl_init(const acl::string& ssl_crt, const acl::string& ssl_key,
 
 static void usage(const char* procname) {
 	printf("usage: %s -h [help]\r\n"
-		" -s listen_addr\r\n"
+		" -s listen_addr[default: 0.0.0.0|1443\r\n"
+		" -t ssl_type[openssl|mbedtls, default: openssl]\r\n"
 		" -L ssl_libs_path\r\n"
-		" -c ssl_crt\r\n"
-		" -k ssl_key\r\n"
+		" -c ssl_crt[default: ../ssl_crt.pem]\r\n"
+		" -k ssl_key[default: ../ssl_key.pem\r\n"
 		, procname);
 }
 
 int main(int argc, char* argv[]) {
-	acl::string addr = "0.0.0.0|2443";
-#if defined(__APPLE__)
-	acl::string ssl_lib = "../libmbedtls.dylib";
-#elif defined(__linux__)
-	acl::string ssl_lib = "../libmbedtls.so";
-#elif defined(_WIN32) || defined(_WIN64)
-	acl::string ssl_path = "../mbedtls.dll";
-
-	acl::acl_cpp_init();
-#else
-# error "unknown OS type"
-#endif
-	acl::string ssl_crt = "../ssl_crt.pem", ssl_key = "../ssl_key.pem";
+	acl::string addr     = "0.0.0.0|1443";
+	acl::string ssl_crt  = "../ssl_crt.pem", ssl_key = "../ssl_key.pem";
+	acl::string ssl_type = "openssl";
+	acl::string ssl_libs;
 
 	int ch;
-	while ((ch = getopt(argc, argv, "hs:L:c:k:")) > 0) {
+	while ((ch = getopt(argc, argv, "hs:t:L:c:k:")) > 0) {
 		switch (ch) {
 		case 'h':
 			usage(argv[0]);
@@ -127,8 +120,11 @@ int main(int argc, char* argv[]) {
 		case 's':
 			addr = optarg;
 			break;
+		case 't':
+			ssl_type = optarg;
+			break;
 		case 'L':
-			ssl_lib = optarg;
+			ssl_libs = optarg;
 			break;
 		case 'c':
 			ssl_crt = optarg;
@@ -141,39 +137,93 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	acl::acl_cpp_init();
 	acl::log::stdout_open(true);
 
-	// 设置 MbedTLS 动态库路径
-	const std::vector<acl::string>& libs = ssl_lib.split2(",; \t");
-	if (libs.size() == 1) {
-		acl::mbedtls_conf::set_libpath(libs[0]);
-	} else if (libs.size() == 3) {
-		// libcrypto, libx509, libssl);
-		acl::mbedtls_conf::set_libpath(libs[0], libs[1], libs[2]);
+	acl::sslbase_conf* ssl_conf;
+
+	if (ssl_type == "mbedtls") {
+		if (ssl_libs.empty()) {
+#if defined(__APPLE__)
+			ssl_libs = "../libmbedtls.dylib";
+#elif defined(__linux__)
+			ssl_libs = "../libmbedtls.so";
+#elif defined(_WIN32) || defined(_WIN64)
+			ssl_libs = "../mbedtls.dll";
+#else
+# error "unknown OS type"
+#endif
+		}
+
+		// 设置 MbedTLS 动态库路径
+		const std::vector<acl::string>& libs = ssl_libs.split2(",; \t");
+		if (libs.size() == 1) {
+			acl::mbedtls_conf::set_libpath(libs[0]);
+		} else if (libs.size() == 3) {
+			// libcrypto, libx509, libssl);
+			acl::mbedtls_conf::set_libpath(libs[0], libs[1], libs[2]);
+		} else {
+			printf("invalid ssl_lib=%s\r\n", ssl_libs.c_str());
+			return 1;
+		}
+
+		// 加载 MbedTLS 动态库
+		if (!acl::mbedtls_conf::load()) {
+			printf("load %s error\r\n", ssl_libs.c_str());
+			return 1;
+		}
+
+		// 初始化服务端模式下的全局 SSL 配置对象
+		bool server_side = true;
+
+		// SSL 证书校验级别
+		acl::mbedtls_verify_t verify_mode = acl::MBEDTLS_VERIFY_NONE;
+
+		ssl_conf = new acl::mbedtls_conf(server_side, verify_mode);
+	} else if (ssl_type == "openssl") {
+#if defined(__APPLE__)
+		acl::string libssl    = "/usr/local/lib/libssl.dylib";
+		acl::string libcrypto = "/usr/local/lib/libcrypto.dylib";
+#elif defined(__linux__)
+		acl::string libssl    = "/usr/local/lib64/libssl.so";
+		acl::string libcrypto = "/usr/local/lib64/libcrypto.so";
+#else
+# error "Unsupport OS!"
+#endif
+		if (!ssl_libs.empty()) {
+			const std::vector<acl::string>& libs = ssl_libs.split2(",; \t");
+			if (libs.size() >= 2) {
+				libssl = libs[0];
+				libcrypto = libs[1];
+			} else {
+				libssl = libs[0];
+			}
+		}
+
+		// 设置 OpenSSL 动态库的加载路径
+		acl::openssl_conf::set_libpath(libcrypto, libssl);
+
+		// 动态加载 OpenSSL 动态库
+		if (!acl::openssl_conf::load()) {
+			printf("load ssl error=%s, crypto=%s, ssl=%s\r\n",
+				acl::last_serror(), libcrypto.c_str(), libssl.c_str());
+			return 1;
+		}
+
+		bool server_side = true;
+		ssl_conf = new acl::openssl_conf(server_side);
 	} else {
-		printf("invalid ssl_lib=%s\r\n", ssl_lib.c_str());
+		printf("Not support ssl type=%s\r\n", ssl_type.c_str());
 		return 1;
 	}
 
-	// 加载 MbedTLS 动态库
-	if (!acl::mbedtls_conf::load()) {
-		printf("load %s error\r\n", ssl_lib.c_str());
-		return 1;
-	}
-
-	// 初始化服务端模式下的全局 SSL 配置对象
-	bool server_side = true;
-
-	// SSL 证书校验级别
-	acl::mbedtls_verify_t verify_mode = acl::MBEDTLS_VERIFY_NONE;
-
-	acl::mbedtls_conf ssl_conf(server_side, verify_mode);
-
-	if (!ssl_init(ssl_crt, ssl_key, ssl_conf)) {
+	if (!ssl_init(ssl_crt, ssl_key, *ssl_conf)) {
 		printf("ssl_init failed\r\n");
 		return 1;
 	}
 
-	start_server(addr, ssl_conf);
+	start_server(addr, *ssl_conf);
+
+	delete ssl_conf;
 	return 0;
 }
