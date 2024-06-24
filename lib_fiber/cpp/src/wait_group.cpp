25b59ab47b99e62d52cd7ede889062225ece29d7
白喵 <478688389@qq.com>
2024-06-24 19:37:43 +0800

    refactor wait_group

--- lib_fiber/cpp/src/wait_group.cpp
+++ lib_fiber/cpp/src/wait_group.cpp
@@ -6,7 +6,7 @@ namespace acl {
 
 wait_group::wait_group(void)
 {
-	count_ = 0;
+	state_ = 0;
 	box_   = new acl::fiber_tbox<unsigned long>;
 }
 
@@ -15,37 +15,60 @@ wait_group::~wait_group(void)
 	delete box_;
 }
 
-void wait_group::add(size_t n)
+void wait_group::add(int n)
 {
-	count_ += n;
+    state_ += (long long)n << 32;
+    long long state = state_;
+    int c = (int)(state >> 32);
+    uint32_t w =  (uint32_t)state;
+    if(c < 0){
+        acl_msg_fatal("wait_group: negative wait_group counter");
+    }
+    if(w != 0 && n > 0 && c == n){
+        acl_msg_fatal("wait_group: add called concurrently with wait");
+    }
+    if(c > 0 || w ==0){
+        return;
+    }
+    if(state_ != state){
+        acl_msg_fatal("wait_group: add called concurrently with wait");
+    }
+    state_ = 0;
+    for (size_t i = 0; i < w; i++) {
+#ifdef	_DEBUG
+        unsigned long* tid = new unsigned long;
+        *tid = acl::thread::self();
+        box_->push(tid);
+#else
+        box_->push(NULL);
+#endif
+    }
 }
 
 void wait_group::done(void)
 {
-#ifdef	_DEBUG
-	unsigned long* tid = new unsigned long;
-	*tid = acl::thread::self();
-	box_->push(tid);
-#else
-	box_->push(NULL);
-#endif
+    add(-1);
 }
 
-size_t wait_group::wait(void)
+void wait_group::wait(void)
 {
-	size_t i;
-	for (i = 0; i < count_; i++) {
-		bool found;
+    long long state = state_;
+    int c = (int)(state >> 32);
+    uint32_t w =  (uint32_t)state;
+    if(c == 0) return;
+    state_++;
+    bool found;
 #ifdef	_DEBUG
-		unsigned long* tid = box_->pop(-1, &found);
-		assert(found);
-		delete tid;
+    unsigned long* tid = box_->pop(-1, &found);
+    assert(found);
+    delete tid;
 #else
-		(void) box_->pop(-1, &found);
-		assert(found);
+    (void) box_->pop(-1, &found);
+    assert(found);
 #endif
-	}
-	return i;
+    if(state_ != 0){
+        acl_msg_fatal("wait_group: wait_group is reused before previous wait has returned");
+    }
 }
 
 } // namespace acl
